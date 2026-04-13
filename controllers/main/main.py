f7f2371b324f8268d0e95c500831065ce6ed042e
Marcio Gabriel <mpereira@cx2.vc>
2026-04-13 16:59:51 +0100

    feat: find red ball

--- controllers/main/main.py
+++ controllers/main/main.py
@@ -1,11 +1,4 @@
-"""
-distance senosors: 0 -> no light is detected, 4095 ->o maximum light is detected
-"""
-import sys
-#sys.path.append(r'C:\Program Files\Webots\lib\controller\python') # add Webots controller library to the path
-from controller import Robot, Motor, Camera, DistanceSensor
-import os
-import math
+from controller import Robot, Motor, Camera
 import numpy as np
 import gymnasium as gym
 from gymnasium import spaces
@@ -14,57 +7,41 @@ import cv2
 
 TIME_STEP = 64
 MAX_SPEED = 6.279
+REACH_THRESHOLD = 0.6
+MAX_STEPS_PER_EPISODE = 500
 
 robot = Robot()
 
-proximity_sensors = []
-PROXIMITY_SENSORS_NAMES = ['ps0', 'ps1', 'ps2', 'ps3', 'ps4', 'ps5', 'ps6', 'ps7']
-
-for index in range(8):
-    proximity_sensors.append(robot.getDevice(PROXIMITY_SENSORS_NAMES[index]))
-    proximity_sensors[index].enable(TIME_STEP)
-
 camera = robot.getDevice('camera')
 camera.enable(TIME_STEP)
-#camera resolution can be set in camera_width and camera_height fields in webots
 
-#initialization of motors
 leftMotor = robot.getDevice('left wheel motor')
 rightMotor = robot.getDevice('right wheel motor')
 
-leftMotor.setPosition(float('inf')) #setting destinatoin posotion - infinity - continuous
+leftMotor.setPosition(float('inf'))
 rightMotor.setPosition(float('inf'))
 
-# set up the motor speeds at 10% of the MAX_SPEED.
-leftMotor.setVelocity(0.15 * MAX_SPEED)
-rightMotor.setVelocity(0.15 * MAX_SPEED)
-
-"""
-
-# init of pen - pen has to be attached for this to work
-pen = robot.getDevice('pen')
-
-# color + intensity of the color
-pen.setInkColor(0xFF0000, 1.0)  # red, full intensity
-# Turn on the writing
-pen.write(True)
-"""
+leftMotor.setVelocity(0.0)
+rightMotor.setVelocity(0.0)
 
 
-
-class MyEnv(gym.Env):
+class RedBallEnv(gym.Env):
     def __init__(self, render_mode=None):
         super().__init__()
 
-        # 2 - left motor and right motor continuous speed control
-        self.action_space = spaces.Box(low=-MAX_SPEED, high=MAX_SPEED, shape=(2,), dtype=np.float32)
+        self.action_space = spaces.Box(
+            low=-MAX_SPEED, high=MAX_SPEED, shape=(2,), dtype=np.float32
+        )
 
+        # ball_x (-1..1), ball_size (0..1), left_speed (-1..1), right_speed (-1..1)
         self.observation_space = spaces.Box(
-            low=0, high=np.inf, shape=(1,), dtype=np.float32
+            low=np.array([-1.0, 0.0, -1.0, -1.0], dtype=np.float32),
+            high=np.array([1.0, 1.0, 1.0, 1.0], dtype=np.float32),
         )
 
         self.render_mode = render_mode
-        self.state = {"previous_red_pixels": 0.0, "current_red_pixels": 0.0}
+        self.current_speeds = np.array([0.0, 0.0], dtype=np.float32)
+        self.steps = 0
 
     def reset(self, seed=None, options=None):
         super().reset(seed=seed)
@@ -73,92 +50,90 @@ class MyEnv(gym.Env):
         leftMotor.setVelocity(0.0)
         rightMotor.setVelocity(0.0)
 
-        self.state = {"previous_red_pixels": 0.0, "current_red_pixels": 0.0}
-        return np.array([0.0], dtype=np.float32), {}
+        self.current_speeds = np.array([0.0, 0.0], dtype=np.float32)
+        self.steps = 0
 
-    def red_pixels_observed(self):
+        ball_x, ball_size = self._detect_ball()
+        observation = np.array(
+            [ball_x, ball_size, 0.0, 0.0], dtype=np.float32
+        )
+        return observation, {}
+
+    def _detect_ball(self):
+        """Return (ball_x, ball_size) from the camera image."""
         raw_image = camera.getImage()
-        if raw_image:
-            width = camera.getWidth()
-            height = camera.getHeight()
-            image = np.frombuffer(raw_image, np.uint8).reshape((height, width, 4))
+        if not raw_image:
+            return 0.0, 0.0
 
-            BGR = image[:, :, :3]
-            # NumPy maska - faster than loop
-            mask = (BGR[:, :, 2] > 130) & (BGR[:, :, 1] < 80) & (BGR[:, :, 0] < 80)
-            count = float(np.count_nonzero(mask))
+        width = camera.getWidth()
+        height = camera.getHeight()
+        image = np.frombuffer(raw_image, np.uint8).reshape((height, width, 4))
 
-            print(f"SEEING {count} RED pixels")
-            cv2.imshow("Robot view red", mask.astype(np.uint8) * 255)
-            cv2.waitKey(1)
+        bgr = image[:, :, :3]
+        mask = (bgr[:, :, 2] > 130) & (bgr[:, :, 1] < 80) & (bgr[:, :, 0] < 80)
 
-            # správný přístup ke slovníku
-            self.state["current_red_pixels"] = count
+        total_pixels = width * height
+        count = int(np.count_nonzero(mask))
+        ball_size = count / total_pixels
 
+        if count == 0:
+            ball_x = 0.0
+        else:
+            cols = np.where(mask)[1]
+            centroid_x = np.mean(cols)
+            ball_x = (centroid_x / width) * 2.0 - 1.0  # normalize to -1..1
+
+        cv2.imshow("Robot view red", mask.astype(np.uint8) * 255)
+        cv2.waitKey(1)
+
+        return float(ball_x), float(ball_size)
 
     def step(self, action):
         robot.step(TIME_STEP)
-        leftMotor.setVelocity(float(action[0]))
-        rightMotor.setVelocity(float(action[1]))
+        self.steps += 1
 
-        self.red_pixels_observed()
+        left_vel = float(np.clip(action[0], -MAX_SPEED, MAX_SPEED))
+        right_vel = float(np.clip(action[1], -MAX_SPEED, MAX_SPEED))
+        leftMotor.setVelocity(left_vel)
+        rightMotor.setVelocity(right_vel)
+        self.current_speeds = np.array(
+            [left_vel / MAX_SPEED, right_vel / MAX_SPEED], dtype=np.float32
+        )
 
-        # odměna podle změny červených pixelů
-        if self.state["current_red_pixels"] > self.state["previous_red_pixels"]:
-            reward = 1   # robot got closer
+        ball_x, ball_size = self._detect_ball()
+
+        if ball_size > 0:
+            centering = 1.0 - abs(ball_x)
+            approach = ball_size * 5.0
+
+            # Reward turning toward the ball:
+            # ball_x < 0 (left) wants right_vel > left_vel (turn left)
+            # ball_x > 0 (right) wants left_vel > right_vel (turn right)
+            turn_rate = (right_vel - left_vel) / (2.0 * MAX_SPEED)
+            steering = -ball_x * turn_rate * 2.0
+
+            reward = centering + approach + steering
         else:
-            reward = -1  # robot got further
+            reward = -2.0
 
-        self.state["previous_red_pixels"] = self.state["current_red_pixels"]
-        observation = np.array([self.state["current_red_pixels"]], dtype=np.float32)
+        terminated = ball_size > REACH_THRESHOLD
+        if terminated:
+            reward += 10.0
 
-        return observation, reward, False, False, {}
+        truncated = self.steps >= MAX_STEPS_PER_EPISODE
+
+        observation = np.array(
+            [ball_x, ball_size, self.current_speeds[0], self.current_speeds[1]],
+            dtype=np.float32,
+        )
+
+        return observation, reward, terminated, truncated, {}
 
     def close(self):
         cv2.destroyAllWindows()
 
 
-
-
-environment = MyEnv()
+environment = RedBallEnv()
 model = PPO("MlpPolicy", environment, verbose=1)
 model.learn(total_timesteps=50000)
 model.save("following_red_ball_model")
-
-
-
-
-"""
-while robot.step(TIME_STEP) != -1:
-    environment.step()
-
-    pass
-"""
-
-
-"""
- # read sensor outputs
-    proximity_sensor_values = []
-    for index in range(8):
-        proximity_sensor_values.append(proximity_sensors[index].getValue())
-
-    right_obstacle: bool = (proximity_sensor_values[0] > 80.0
-                            or proximity_sensor_values[1] > 80.0
-                            or proximity_sensor_values[2] > 80.0)
-    left_obstacle: bool = (proximity_sensor_values[5] > 80.0
-                            or proximity_sensor_values[6] > 80.0
-                            or proximity_sensor_values[7] > 80.0)
-
-    left_speed = 0.5 * MAX_SPEED
-    right_speed = 0.5 * MAX_SPEED
-
-    if left_obstacle:
-        print("Left obstacle detected, turning right")
-        #left_speed = 0.5 * MAX_SPEED
-        #right_speed = -0.5 * MAX_SPEED
-
-    if right_obstacle:
-        print("Right obstacle detected, turning left")
-        #left_speed = -0.5 * MAX_SPEED
-        #right_speed = 0.5 * MAX_SPEED
-"""
